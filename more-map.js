ce78d7d56fe43e06c964723f5bda159d3be35cb0
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-09 01:47:59 +0600

    more map from an array

--- more-map.js
+++ more-map.js
@@ -9,7 +9,22 @@ const thirds = numbers.map(n => n / 3);
 // Get Strings value from an Array using Map
 const friends = ['Tom Cruise', 'Angilina Joly', 'Chris Evan', 'Black Widow', 'Thor', 'Iron Man'];
 const firstLetters = friends.map(friend => friend[0]);
-console.log(firstLetters);
+// console.log(firstLetters);
 
 const nameLengths = friends.map(friend => friend.length);
-console.log(nameLengths);
+// console.log(nameLengths);
+
+const products = [
+    {id: 1, name: 'Laptop', price: 45000},
+    {id: 2, name: 'mobile', price: 80000},
+    {id: 3, name: 'ipad', price: 38000},
+    {id: 4, name: 'tablet', price: 29000},
+    {id: 5, name: 'watch', price: 12000},
+];
+
+const product = products.map(product => product);
+const items = products.map(price => price.price);
+const itemName = products.map(name => name.name);
+// console.log(product);
+// console.log(items)
+console.log(itemName)
