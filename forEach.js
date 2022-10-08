939ca9beb62c296499eb63cf49508bf4e2a858f2
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-09 01:51:01 +0600

    try for each in js

---
+++ forEach.js
@@ -0,0 +1,14 @@
+const products = [
+    {id: 1, name: 'Laptop', price: 45000},
+    {id: 2, name: 'mobile', price: 80000},
+    {id: 3, name: 'ipad', price: 38000},
+    {id: 4, name: 'tablet', price: 29000},
+    {id: 5, name: 'watch', price: 12000},
+];
+
+products.forEach(items => console.log(items.name));
+
+
+const friends = ['Tom Cruise', 'Angilina Joly', 'Chris Evan', 'Black Widow', 'Thor', 'Iron Man'];
+
+friends.forEach(friend => console.log(friend));
