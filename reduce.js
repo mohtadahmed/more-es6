ef628c5d27d9cd0db7d9f65b065bafe97f31fba4
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-09 12:34:49 +0600

    explore reduce in js

---
+++ reduce.js
@@ -0,0 +1,10 @@
+const numbers = [10, 20, 30, 40];
+
+const total = numbers.reduce((previous, current) => previous + current, 0);
+// console.log(total);
+
+
+const names = ['Karim', 'Abul', 'Rony', 'Bunny', 'Honey'];
+
+const nameTotal = names.reduce((previous, current) => previous + current, 'Hi');
+console.log(nameTotal);
