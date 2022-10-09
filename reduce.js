6598761ae338db9487774f9a6dc2a49d3af35a82
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-09 13:08:31 +0600

    using reduce with multiline function

--- reduce.js
+++ reduce.js
@@ -1,6 +1,12 @@
 const numbers = [10, 20, 30, 40];
 
+// Using reduce with single line arrow function
 const total = numbers.reduce((previous, current) => previous + current, 0);
+
+// Using reduce with multiline arrow function
+const newTotal = numbers.reduce((previous, current) => {
+    return previous + current;
+}, 0)
 // console.log(total);
 
 
